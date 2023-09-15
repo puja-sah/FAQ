879627a5647cfefcc7c0f35be5cdd4491986dd7e
PujA_SaH <126154361+puja-sah@users.noreply.github.com>
2023-09-16 00:22:13 +0530

    FAQ'S UPDATED

---
+++ faq.js
@@ -0,0 +1,22 @@
+const accordian = document.querySelectorAll('.accordian');    // access all css accordians
+accordian.forEach(accordian => {
+    const icon = accordian.querySelector('.icon');
+    const answer = accordian.querySelector('.answer');
+
+
+    accordian.addEventListener('click', () => {
+        
+
+        if(icon.classList.contains('active')){
+            icon.classList.remove('active');
+            answer.style.maxHeight = null;
+        }
+        else{
+            icon.classList.add('active');
+            answer.style.maxHeight = answer.scrollHeight + 'px';
+
+        }
+
+    })
+
+})
